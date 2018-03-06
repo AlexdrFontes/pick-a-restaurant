6d324799428f3bbbf38e70f85adf1d42ed740dcc
Alexandre Fontes <alexandrefontes90@gmail.com>
2018-03-06 15:49:16 +0000

    search_page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,18 @@
 class PlacesController < ApplicationController
-  def show
-  end
+  skip_before_action :authenticate_user!, only: [:search, :show]
 
-  def search
+  def show
+    @places = Place.all
+    if params[:cuisine_type].present?
+      sql_query = " \
+        cuisine_types.name @@ :cuisine_type \
+       "
+      @places = @places.joins(:cuisine_types).where(sql_query, cuisine_type: "%#{params[:cuisine_type]}%")
+    end
+
+    if params[:location].present?
+      @places = @places.where(city: params[:location].capitalize)
+    end
   end
 end
+# OR places.city @@ :search \
