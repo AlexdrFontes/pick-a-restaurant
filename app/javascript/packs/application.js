2680fc9b7a79c38f6872b5c5ebf531844007d8d9
Alexandre Fontes <alexandrefontes90@gmail.com>
2018-03-13 16:00:13 +0000

    autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-// import { autocomplete } from 'autocomplete';
+import { autocomplete } from '../components/autocomplete';
 // autocomplete();
 // import "range_slider";
 
@@ -7,3 +7,4 @@ import "bootstrap";
 
 // (function(){function t(t){function n(t){var n=(new Date).getTime()-a;e.duration>n?clearTimeout(i):t.target.dispatchEvent(o),r(t)}function r(t){t.target.removeEventListener("mouseup",n),t.target.removeEventListener("touchstop",n)}var a=(new Date).getTime();t.target.addEventListener("mouseup",n),t.target.addEventListener("touchstop",n);var i=setTimeout(function(){t.target.dispatchEvent(o),r(t)},e.duration)}var e={duration:500};if("object"==typeof longpress_config)for(var n in longpress_config)e[n]=longpress_config[n];var o=document.createEvent("CustomEvent");o.initCustomEvent("longpress",!0,!0,{});var r=document.body;r.addEventListener("mousedown",t),r.addEventListener("touchstart",t)})();
 
+autocomplete();
