23f89e1100dc20ee7e5566dc1a746ab9416ed751
Alexandre Fontes <alexandrefontes90@gmail.com>
2018-03-13 19:16:50 +0000

    error_404

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,16 +9,15 @@ class PlacesController < ApplicationController
 
  end
 
- def show
-
-
-  @places = Place.all
-  @cuisine_types = CuisineType.all
-  @unique_types = @cuisine_types.select(:id,:name).uniq{|a| a.name}.map{ |a| [a.name,a.id]}
+  def show
+    @places = Place.all
+    @cuisine_types = CuisineType.all
+    @unique_types = @cuisine_types.select(:id,:name).uniq{|a| a.name}.map{ |a| [a.name,a.id]}
 
     if params.values_at(:place, :rangeslider).all?(&:present?)
       @places = Place.near(params[:place], params[:rangeslider])
     end
+
     if params[:meal_type].present?
       sql_query = " \
       meal_types.name @@ :meal_type \
@@ -26,65 +25,54 @@ class PlacesController < ApplicationController
       @places = @places.joins(:meal_types).where(sql_query, meal_type: "%#{params[:meal_type]}%")
     end
 
-
-  if params[:search] && params[:search][:cuisine_types].any? && params[:search][:cuisine_types][1].present?
-
+    if params[:search] && params[:search][:cuisine_types].any? && params[:search][:cuisine_types][1].present?
     sql_query = " \
     cuisine_types.name @@ :cuisine_type \
     "
-
-
     @places = @places.where(place_cuisine_types: { cuisine_type_id: params[:search][:cuisine_types] }).includes(:place_cuisine_types).references(:place_cuisine_types)
+    end
 
-  end
-
-  if params[:min_price].present?
+    if params[:min_price].present?
        # @places = @places.joins(:places).where(sql_query, min_price: "%#{params[:min_price]}%")
        min = params[:min_price].to_i
        @places = @places.where("average_cost_for_two >= ?",min)
-     end
+    end
 
-     if params[:max_price].present?
+    if params[:max_price].present?
        # @places = @places.joins(:places).where(sql_query, min_price: "%#{params[:min_price]}%")
        max = params[:max_price].to_i
        @places = @places.where("average_cost_for_two <= ?",max)
-     end
+    end
 
+    if @places.empty?
+      redirect_to error_404_path
+    else
      @place = @places.sample
 
      @place.places_histories.create(user: current_user)
-
+    end
       # @places = @places.where(address: params[:address])
-    end
-
-    def show_id
-      @place = Place.find(params[:id])
-      render "show"
-    end
-
-    def my_places
-
-      @places = current_user.places_histories.order(:id).map(&:place).reverse
-
-
-
-    end
-
-    def save_id
-      @place = PlacesHistory.where(user_id: current_user, place_id: params[:id])
-
-      if @place.first.rejected == false
-        @place.first.update(rejected: true)
-      elsif @place.first.rejected == nil
-        @place.first.update(rejected: true)
-      else
-        @place.first.update(rejected: false)
-      end
-
-      @places = current_user.places
-      redirect_to places_my_places_path
-
-    end
-
-
   end
+
+  def show_id
+    @place = Place.find(params[:id])
+    render "show"
+  end
+
+  def my_places
+    @places = current_user.places_histories.order(:id).map(&:place).reverse
+  end
+
+  def save_id
+    @place = PlacesHistory.where(user_id: current_user, place_id: params[:id])
+    if @place.first.rejected == false
+      @place.first.update(rejected: true)
+    elsif @place.first.rejected == nil
+      @place.first.update(rejected: true)
+    else
+      @place.first.update(rejected: false)
+    end
+    @places = current_user.places
+    redirect_to places_my_places_path
+  end
+end
