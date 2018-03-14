60bd0ccfbdcacb8cca78649caea28711f485a0a6
Alexandre Fontes <alexandrefontes90@gmail.com>
2018-03-14 16:50:27 +0000

    lisbon_search

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -2,8 +2,13 @@ function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var placeAddress = document.getElementById('place_address');
 
+    const options = {
+      types: [ 'geocode' ],
+      componentRestrictions: {country: "pt"}
+    }
+
     if (placeAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(placeAddress, { types: [ 'geocode' ] });
+      var autocomplete = new google.maps.places.Autocomplete(placeAddress, options);
       google.maps.event.addDomListener(placeAddress, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
